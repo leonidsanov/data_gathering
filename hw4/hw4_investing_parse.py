363d708a7f6068e1d0fb88aadf226c142a72abe8
Leonid Sanov <sanov.87@mail.ru>
2024-01-31 23:16:28 +0500

    hw4_done

--- hw4/hw4_investing_parse.py
+++
@@ -1,8 +0,0 @@
-# Импорт необходимых библиотек
-import requests
-from lxml import html
-from pymongo import MongoClient
-
-# Определение целевого URL
-url = "https://ru.investing.com/etfs/russia-etfs?&issuer_filter=0"
-
